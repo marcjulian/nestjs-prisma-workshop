bcca40b62ff7020c4184d9f656efe82321f109b2
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2021-06-23 16:31:31 +0200

    export prisma service and extend prisma service with prisma client

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
 @Module({
-  providers: [PrismaService]
+  providers: [PrismaService],
+  exports: [PrismaService], // 👈 export PrismaService for DI
 })
 export class PrismaModule {}
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient {
+  constructor() {
+    // pass PrismaClientOptions e.g. logging levels or error formatting
+    super();
+  }
+}
