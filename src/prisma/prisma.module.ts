42c3dd4603c3e51bf3cbfddbea314bb50e407929
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2021-06-23 17:33:39 +0200

    make prismamodule global

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,6 +1,7 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
+@Global() // 👈 makes PrismaService globally available
 @Module({
   providers: [PrismaService],
   exports: [PrismaService], // 👈 export PrismaService for DI
