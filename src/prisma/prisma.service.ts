93ac597349b95fcabf517db80b10ee87514fa675
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2021-06-24 15:58:59 +0200

    explicit connect and disconnect prisma from db

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,10 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   constructor() {
     // pass PrismaClientOptions e.g. logging levels or error formatting
     super();
+  }
+
+  // optionally if the first request need to respond instantly
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
