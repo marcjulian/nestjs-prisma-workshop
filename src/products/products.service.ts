3176e2cbcb1862818f39371085df6385353963d6
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2021-06-24 16:03:34 +0200

    inject prisma service

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaService } from './../prisma/prisma.service';
 
 @Injectable()
 export class ProductsService {
+  constructor(private prisma: PrismaService) {}
+
   create(createProductDto: CreateProductDto) {
     return 'This action adds a new product';
   }
