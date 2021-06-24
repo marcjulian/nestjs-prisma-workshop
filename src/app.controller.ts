8d87a6da49a825d15979885728e0980c4e88db25
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2021-06-24 11:35:32 +0200

    get all published products

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private prisma: PrismaService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('products')
+  findPublishedProducts() {
+    return this.prisma.product.findMany({
+      where: { published: true },
+    });
   }
 }
