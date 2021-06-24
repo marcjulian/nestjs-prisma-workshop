1c045ad4567921223b8a28adc4fb875b9d8cd419
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2021-06-24 17:18:13 +0200

    annotate with @ApiProperty

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,7 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateProductDto {
+  @ApiProperty()
   name: string;
+  @ApiProperty()
   description?: string;
+  @ApiProperty()
   price: number;
+  @ApiProperty()
   sku: string;
+  @ApiProperty()
   published?: boolean = false;
 }
