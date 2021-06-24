ff541c70a5fb64d0fc845778fa859898e69183cc
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2021-06-24 21:52:23 +0200

    add response type

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,33 +1,48 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { ProductEntity } from './entities/product.entity';
+import { ApiCreatedResponse, ApiOkResponse } from '@nestjs/swagger';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
+  @ApiCreatedResponse({ type: ProductEntity })
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
   @Get()
+  @ApiOkResponse({ type: [ProductEntity] })
   findAll() {
     return this.productsService.findAll();
   }
 
   @Get(':id')
+  @ApiOkResponse({ type: ProductEntity })
   findOne(@Param('id') id: string) {
     return this.productsService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiCreatedResponse({ type: ProductEntity })
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(id, updateProductDto);
   }
 
   @Delete(':id')
+  @ApiOkResponse({ type: ProductEntity })
   remove(@Param('id') id: string) {
     return this.productsService.remove(id);
   }
