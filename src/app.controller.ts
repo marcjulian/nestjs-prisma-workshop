58047a7299e5d6a905eaf21bf0c99bdd4a305116
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2021-06-24 16:06:53 +0200

    remove products call

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,23 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { PrismaService } from './prisma/prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private prisma: PrismaService,
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-
-  @Get('products')
-  findPublishedProducts() {
-    return this.prisma.product.findMany({
-      where: { published: true },
-    });
   }
 }
