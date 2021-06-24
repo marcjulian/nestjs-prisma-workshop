e4d00f286f14ebd616154156361eaa278946e15e
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2021-06-24 11:54:59 +0200

    product seeds

---
+++ products.ts
@@ -0,0 +1,107 @@
+// database product seed
+export const products = [
+  {
+    name: "SUP Board 12'",
+    description: "Inflatable SUP Board 12' in RedOrange.",
+    price: 549.99,
+    sku: 'sku_supboard_12_red',
+    published: true,
+  },
+  {
+    name: "SUP Board 14'",
+    description: "Inflatable SUP Board 14' in all new Blue color.",
+    price: 629.95,
+    sku: 'sku_sup_board14_blue',
+    published: true,
+  },
+  {
+    name: "SUP Board 10' Youth",
+    description:
+      'Short and narrow touring board for children, perfect for fast paddling.',
+    price: 550.95,
+    sku: 'sku_sup_board10_youth',
+    published: true,
+  },
+  {
+    name: "SUP Board 10'",
+    description: 'New short and narrow touring board.',
+    price: 570,
+    sku: 'sku_sup_board10',
+    published: false,
+  },
+  {
+    name: 'SUP Glass Paddle',
+    price: 79.0,
+    sku: 'sku_sup_paddle_glass',
+    published: true,
+  },
+  {
+    name: 'SUP Carbon Paddle',
+    description: 'Perfect suited for racing.',
+    price: 159.49,
+    sku: 'sku_sup_paddle_carbon',
+    published: true,
+  },
+  {
+    name: 'SUP NEW Carbon Paddle',
+    description: 'Lighter than ever.',
+    price: 229.0,
+    sku: 'sku_sup_paddle_new_carbon',
+    published: false,
+  },
+  {
+    name: 'SUP Paddle Kids',
+    description: 'Suitable for kids from 7 to 12 years.',
+    price: 59.0,
+    sku: 'sku_sup_paddle_kids',
+    published: true,
+  },
+  {
+    name: 'Dry Bag 10L Red',
+    description: '10 Liter dry bag keeping your valuables dry.',
+    price: 17.99,
+    sku: 'sku_dry_bag_10l_red',
+    published: true,
+  },
+  {
+    name: 'Dry Bag 10L Black',
+    description: '10 Liter dry bag keeping your valuables dry.',
+    price: 15.99,
+    sku: 'sku_dry_bag_10l_black',
+    published: true,
+  },
+  {
+    name: 'Dry Bag 35L',
+    description: '35 Liter dry bag keeping your valuables and clothes dry.',
+    price: 23.99,
+    sku: 'sku_dry_bag_35l',
+    published: true,
+  },
+  {
+    name: 'Dry Bag 100L',
+    description: '100 Liter dry bag perfect for long trips.',
+    price: 36.99,
+    sku: 'sku_dry_bag_100l',
+    published: true,
+  },
+  {
+    name: 'Dry Bag 50L',
+    description:
+      '50 Liter dry bag extra room to carry valuables of your friends.',
+    price: 30.99,
+    sku: 'sku_dry_bag_50l',
+    published: false,
+  },
+  {
+    name: 'Flex Touring Fin Blue',
+    price: 26.9,
+    sku: 'sku_fin_flex_touring_blue',
+    published: true,
+  },
+  {
+    name: 'Touring Fin Red',
+    price: 20.9,
+    sku: 'sku_fin_touring_red',
+    published: false,
+  },
+];
