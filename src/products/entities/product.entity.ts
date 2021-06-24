cfb2ffddc123b162be31742810bbae1b8e565cd5
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2021-06-24 21:50:07 +0200

    create product entity

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,20 @@
-export class Product {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class ProductEntity {
+  @ApiProperty()
+  id: string;
+  @ApiProperty()
+  createdAt: Date;
+  @ApiProperty()
+  updatedAt: Date;
+  @ApiProperty()
+  name: string;
+  @ApiProperty({ required: false, nullable: true })
+  description?: string;
+  @ApiProperty()
+  price: string;
+  @ApiProperty()
+  sku: string;
+  @ApiProperty({ default: false })
+  published: boolean;
+}
