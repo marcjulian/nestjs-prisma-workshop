1eea1e3ea2dbd9900a960338f35e680829d3b0cb
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2021-06-24 16:11:40 +0200

    update swagger api title

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('NestJS Prisma Workshop')
-    .setDescription('API description')
+    .setTitle('Prisma Day - NestJS Prisma Workshop')
+    .setDescription('Building a REST API with NestJS and Prisma')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, { customSiteTitle: 'Prisma Day' });
 
   await app.listen(3000);
 }
