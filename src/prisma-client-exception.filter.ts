31ccac0e69e4b4ce5b796ce4e26d209a1a138d9b
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2021-06-25 18:29:43 +0200

    create conflict response

--- src/prisma-client-exception.filter.ts
+++ src/prisma-client-exception.filter.ts
@@ -1,14 +1,24 @@
-import { ArgumentsHost, Catch } from '@nestjs/common';
+import { ArgumentsHost, Catch, HttpStatus } from '@nestjs/common';
 import { BaseExceptionFilter } from '@nestjs/core';
 import { Prisma } from '@prisma/client';
+import { Response } from 'express';
 
 @Catch(Prisma.PrismaClientKnownRequestError)
 export class PrismaClientExceptionFilter extends BaseExceptionFilter {
   catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
     switch (exception.code) {
       case 'P2002':
-        // TODO return conflict response
+        const status = HttpStatus.CONFLICT;
+        const message = exception.message.replace(/\n/g, '');
+        response.status(status).json({
+          statusCode: status,
+          message: message,
+        });
         break;
+      // TODO catch other error codes (e.g. 'P2000' or 'P2025')
       default:
         // default 500 error code
         super.catch(exception, host);
