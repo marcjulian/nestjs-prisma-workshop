1cf7cb3520c958421674accc8eedbee13107bb06
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2021-06-25 10:12:38 +0200

    validation

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,14 +1,32 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsNotEmpty,
+  IsOptional,
+  MaxLength,
+  Min,
+  MinLength,
+} from 'class-validator';
 
 export class CreateProductDto {
+  @IsNotEmpty()
+  @MinLength(3)
   @ApiProperty()
   name: string;
+
+  @IsOptional()
+  @MaxLength(150)
   @ApiProperty({ required: false })
   description?: string;
+
+  @Min(1.0)
   @ApiProperty()
   price: number;
+
+  @IsNotEmpty()
+  @MinLength(5)
   @ApiProperty()
   sku: string;
+
   @ApiProperty({ required: false, default: true })
   published?: boolean = false;
 }
