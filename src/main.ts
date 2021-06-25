5b794aadde90a2a5b62d736c7cfba6cdaf67fe37
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2021-06-25 09:16:17 +0200

    bind validationpipe at application level

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // binds ValidationPipe to the entire application
+  app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
     .setTitle('Prisma Day - NestJS Prisma Workshop')
