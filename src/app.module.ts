78d02e72ec8d39755c706ab49c971a11ec44a49b
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2021-06-27 22:29:00 +0200

    generate auth resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProductsModule } from './products/products.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, ProductsModule],
+  imports: [PrismaModule, ProductsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
