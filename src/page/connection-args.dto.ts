83a29340be5ccb8731e5fda70d4cbb71fc2ba060
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2021-06-27 16:14:59 +0200

    validate first and last as optional and number

--- src/page/connection-args.dto.ts
+++ src/page/connection-args.dto.ts
@@ -1,12 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
+import { IsNumber, IsOptional } from 'class-validator';
 
 export class ConnectionArgs {
+  @IsOptional()
+  @IsNumber()
   // 👇 use if enableImplicitConversion not enabled
   // @Type(() => Number)
   @ApiProperty({ required: false })
   first?: number;
-  
+
+  @IsOptional()
+  @IsNumber()
   // 👇 use if enableImplicitConversion not enabled
   // @Type(() => Number)
   @ApiProperty({ required: false })
