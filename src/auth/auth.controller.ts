bd043a0c83fc7dc9e53fbcbf39706ed8a29ae83f
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2021-06-27 22:47:36 +0200

    login endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { ApiOkResponse } from '@nestjs/swagger';
+import { Auth } from './entity/auth.entity';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  @ApiOkResponse({ type: Auth })
+  login(@Body() { email, password }: LoginDto) {
+    return this.authService.login(email, password);
+  }
 }
