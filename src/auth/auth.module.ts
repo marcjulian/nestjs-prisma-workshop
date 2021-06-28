bef6d95b3987fd95c3a7b29a6058b170573b1a4b
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2021-06-28 09:49:50 +0200

    import passport and configure jwt module with secret key

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+
+// don't expose your keys, move them into .env file
+// read values using the @nestjs/config package
+// https://docs.nestjs.com/techniques/configuration
+export const jwtSecret = 'prismaDay2021';
 
 @Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: jwtSecret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
