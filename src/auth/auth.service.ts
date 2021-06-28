45288bce4cad27be0a49550fe41d3f3b9c6aa35a
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2021-06-28 12:14:13 +0200

    implement login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Auth } from './entity/auth.entity';
+import { PrismaService } from './../prisma/prisma.service';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  async login(email: string, password: string) {}
+  constructor(private prisma: PrismaService, private jwtService: JwtService) {}
+
+  async login(email: string, password: string): Promise<Auth> {
+    const user = await this.prisma.user.findUnique({ where: { email: email } });
+
+    if (!user) {
+      throw new NotFoundException(`No user found for email: ${email}`);
+    }
+
+    // TODO use a library like bcrypt to hash and compare your passwords
+    // https://github.com/kelektiv/node.bcrypt.js#readme
+    const passwordValid = user.password === password;
+
+    if (!passwordValid) {
+      throw new UnauthorizedException('Invalid password');
+    }
+
+    return {
+      accessToken: this.jwtService.sign({ userId: user.id }),
+    };
+  }
 }
