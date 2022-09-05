bc82159974ee49fdf89f07e05953b9d6fbbf6dec
FadFlex <fsiddiqui107@gmail.com>
2022-09-05 16:46:56 +0500

    work

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -7,7 +7,6 @@ import {
   Param,
   Delete,
   Req,
-  
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
