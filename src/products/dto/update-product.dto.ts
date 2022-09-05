22e750bc97692fb3cd40640e71668245d6975562
FadFlex <fsiddiqui107@gmail.com>
2022-09-05 16:44:33 +0500

    work

--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -1,6 +1,4 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateProductDto } from './create-product.dto';
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {
-    
-}
+export class UpdateProductDto extends PartialType(CreateProductDto) {}
