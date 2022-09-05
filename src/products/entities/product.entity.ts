3bafca44a0a87f118705a697a760636dcb07afa7
FadFlex <fsiddiqui107@gmail.com>
2022-09-05 16:43:33 +0500

    work complete

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,29 +1,28 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { Product,Prisma } from "@prisma/client";
+import { ApiProperty } from '@nestjs/swagger';
+import { Product, Prisma } from '@prisma/client';
 
 export class ProductEntity implements Product {
+  @ApiProperty()
+  id: string;
 
-    @ApiProperty()
-    id:string; 
-    
-    @ApiProperty()   
-    createdAt:Date;
+  @ApiProperty()
+  createdAt: Date;
 
-    @ApiProperty()
-    updatedAt: Date;
+  @ApiProperty()
+  updatedAt: Date;
 
-    @ApiProperty()
-    name:string;
+  @ApiProperty()
+  name: string;
 
-    @ApiProperty({required:false,nullable:true})
-    description: string;
+  @ApiProperty({ required: false, nullable: true })
+  description: string;
 
-    @ApiProperty({type:String})
-    price: Prisma.Decimal;
+  @ApiProperty({ type: String })
+  price: Prisma.Decimal;
 
-    @ApiProperty()
-    sku:string;
+  @ApiProperty()
+  sku: string;
 
-    @ApiProperty({default:false})
-    published:boolean;
+  @ApiProperty({ default: false })
+  published: boolean;
 }
